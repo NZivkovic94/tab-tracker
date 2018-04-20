3b5117f56e1bfcc7e04a1164c3ea00eb383e6b18
NZivkovic <nzivkovic@ubuntu.ubuntu-domain>
2018-04-20 17:59:38 +0200

    add register endpoint

---
+++ client/src/components/Register.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <h1>Register</h1>
+  <input
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email"/>
+  <br>
+  <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password"/>
+  <br>
+  <button>Register</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      email: 'abc',
+      password: '123'
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed', value)
+    }
+  }
+}
+</script>
+<style>
+
+</style>
