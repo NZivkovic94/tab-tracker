94f70452f29f088cbf01a08238358486662b484e
NZivkovic <nzivkovic@ubuntu.ubuntu-domain>
2018-04-20 16:55:49 +0200

    bug fix 1.0

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -15,4 +15,4 @@ app.get('/status', (req, res) => {
   })
 })
 
-app.listen(process.env.PORT)
+app.listen(process.env.PORT || 8081)
